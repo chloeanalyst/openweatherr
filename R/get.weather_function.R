171ab6e3cdbe6d376270858c877cfd7516e29ebe
Chloe Morgan <52977984+chloeanalyst@users.noreply.github.com>
2021-09-11 18:28:48 +0100

    Initial commit

---
+++ R/get.weather_function.R
@@ -0,0 +1,46 @@
+#' OpenWeatherMapR
+#'
+#' Returns weather metrics for specified Location and Date Range from the Open Weather Map API
+#'
+#' This is a descriptions
+#' desctiptions
+#' blah blah blah
+#'
+#'
+#' @param key string Your Open Weather Map API key.
+#' @param location City name eg: Manchester.
+#' @param date Date range: today, yesterday, last5days, "2021-07-01" or list of dates example - list("2021-07,01","2021-07-02")
+#'
+#' @author Chloe Morgan
+#'
+#' @import httr
+#' @import jsonlite
+#' @import data.table
+#' @import anytime
+#' @import dplyr
+#'
+#' @return Table containing weather information.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' get.weather("APIKEY", Manchester,today)
+#' get.weather("APIKEY", Manchester, "2021-07-01")
+#' }
+
+
+get.weather <- function(key,location,daterange){
+
+  result <- list()
+
+  for (i in daterange){
+
+    result[[i]] <- setup.weather(key,location,i)
+
+  }
+
+  result <- as.data.frame(do.call(rbind,result)) %>% arrange(desc(date))
+
+  return(result)
+
+}
---
+++ R/setup.weather_function.R
@@ -0,0 +1,51 @@
+#' OpenWeatherMapR
+#'
+#' Returns weather metrics for specified Location and Date Range from the Open Weather Map API
+#'
+#' This is a descriptions
+#' desctiptions
+#' blah blah blah
+#'
+#'
+#' @param key string Your Open Weather Map API key.
+#' @param location City name eg: Manchester.
+#' @param date Date range: today, yesterday, last5days, "2021-07-01"
+#'
+#' @author Chloe Morgan
+#'
+#' @import httr
+#' @import jsonlite
+#' @import data.table
+#' @import anytime
+#' @import dplyr
+#'
+#' @return Table containing weather information.
+#'
+#'
+#' @examples
+#' \dontrun{
+#' setup.weather("APIKEY", Manchester,today)
+#' setup.weather("APIKEY", Manchester, "2021-07-01")
+#' }
+
+setup.weather <- function(key,location,date){
+
+  apikey <- key
+  date_POSIXct <- as.numeric(as.POSIXct(date))
+  URL <- paste0('https://api.openweathermap.org/data/2.5/onecall/timemachine?',location,'&dt=',date_POSIXct,'&appid=',apikey)
+  res <- GET(URL)
+  data <- fromJSON(rawToChar(res$content))
+  dataRes <- data$hourly$weather
+  datalist <- rbindlist(dataRes, fill = TRUE)
+  datalist$date <- date
+  datalist$hour <- seq.int(nrow(datalist))
+  datahourly <- data$hourly
+  datahourly <- datahourly %>% select(temp,wind_speed)
+  datahourly$temp <- datahourly$temp - 273.15                                  # Convert temp from Kelvin to C'
+  datalist <- cbind(datalist, datahourly)
+
+  datalist <- datalist %>% select(date, hour, temp, main, description, wind_speed)
+
+  return(datalist)
+}
+
